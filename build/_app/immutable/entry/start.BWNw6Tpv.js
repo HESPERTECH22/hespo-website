a4102af0bc2a3be1a10b0e039ac4b97a4488c139
arj101 <arjunanmr101@gmail.com>
2026-06-12 17:45:20 +0530

    first commit

---
+++ build/_app/immutable/entry/start.BWNw6Tpv.js
@@ -0,0 +1,1 @@
+import{o as e,t}from"../chunks/DSVdg1Jh.js";export{e as load_css,t as start};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+
+
+<main class="w-full h-full grid place-items-center bg-[#F0F7FB] relative">
+    <div class="flex flex-col items-center z-2">
+        <img src="./logo.svg" class="h-16 md:h-24 lg:h-28 "/>
+        <h1 class="text-md md:text-lg lg:text-2xl font-light ">Parcel pooling services</h1>
+        <h1 class="text-3xl m-12 font-light text-[#6192AA]">Coming soon...</h1>
+    </div>
+
+    <div class="absolute left-0 bottom-0 w-full grid place-items-center z-0">
+        <div class="w-fit h-fit img-wrapper">
+        <img src="./graphic.svg"/>
+        </div>
+    </div>
+
+    <div class="absolute w-full left-0 bottom-0 z-0 grid place-items-center ">
+        <img src="./hesper-logo.png" class="h-20 md:h-24 lg:h-24"/>
+    </div>
+</main>
+
+<style>
+    * {
+        font-family: 'Geist', sans-serif;
+        transition: all 300ms ease;
+    }
+    .img-wrapper{
+      -webkit-mask-image: linear-gradient(
+        to right,
+        transparent 10%,
+        black 20%,
+        black 80%,
+        transparent 90%
+      );
+      mask-image: linear-gradient(
+        to right,
+        transparent 10%,
+        black 20%,
+        black 80%,
+        transparent 90%
+      );
+    }
+
+
+
+</style>
